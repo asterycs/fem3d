a1cbdf84d66a4bf68efb79c60159516676adb932
Jimmy Envall <jimmyenvall@gmail.com>
2019-02-21 23:48:55 +0200

    More on fem task with linear bases.

--- src/FEMTask3D.cpp
+++ src/FEMTask3D.cpp
@@ -10,17 +10,26 @@ FEMTask3D::FEMTask3D(const std::vector<Vector3>& vertices, const std::vector<Uns
 
     for (UnsignedInt ti = 0; ti < tetrahedronIds.size(); ti += 4)
     {
-        const UnsignedInt vi0 = tetrahedronIds[ti  ];
-        const UnsignedInt vi1 = tetrahedronIds[ti+1];
-        const UnsignedInt vi2 = tetrahedronIds[ti+2];
-        const UnsignedInt vi3 = tetrahedronIds[ti+3];
+        const UnsignedInt vi[] = {tetrahedronIds[ti],
+                                  tetrahedronIds[ti+1],
+                                  tetrahedronIds[ti+2],
+                                  tetrahedronIds[ti+3]};
 
-        const Eigen::Vector3f p0 = toEigen(_vertices[vi0]);
-        const Eigen::Vector3f p1 = toEigen(_vertices[vi1]);
-        const Eigen::Vector3f p2 = toEigen(_vertices[vi2]);
-        const Eigen::Vector3f p3 = toEigen(_vertices[vi3]);
+        const Eigen::Vector3f p0 = toEigen(_vertices[vi[0]]);
+        const Eigen::Vector3f p1 = toEigen(_vertices[vi[1]]);
+        const Eigen::Vector3f p2 = toEigen(_vertices[vi[2]]);
+        const Eigen::Vector3f p3 = toEigen(_vertices[vi[3]]);
 
+        Eigen::Matrix3f Bk; Bk << p1 - p0, p2 - p0, p3 - p0;
+        Eigen::Vector3f bk = p0;
 
+        for (UnsignedInt i = 0; i < 4; ++i)
+        {
+            for (UnsignedInt j = 0; j < 4; ++j)
+            {
+                //A(vi[i],vi[j]) += ...
+            }
+        }
     }*/
 }
 
