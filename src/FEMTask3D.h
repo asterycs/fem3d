03b758c080973889c46cec49fb3a95d318352161
Jimmy Envall <jimmyenvall@gmail.com>
2019-02-25 23:13:30 +0200

    Very ugly and unpolished solver for laplace with linear bases.

--- src/FEMTask3D.h
+++ src/FEMTask3D.h
@@ -16,15 +16,19 @@ class FEMTask3D
 {
 public:
     explicit FEMTask3D(const std::vector<Vector3>& vertices, const std::vector<UnsignedInt>& tetrahedronIds, const std::set<UnsignedInt>& pinnedVertexIds);
-    //std::vector<Float> solve();
+
+    std::vector<Float> solve();
 
 
 private:
+    void initialize();
+
     std::vector<Vector3> _vertices;
     std::vector<UnsignedInt> _tetrahedronIndices;
     std::set<UnsignedInt> _pinnedVertexIds;
 
     Eigen::SparseMatrix<Float> _A;
+    Eigen::SparseVector<Float> _b;
 };
 
 
