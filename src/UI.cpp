12849b52b506adb751f0b721a54b0fe3ec9f30ec
Jimmy Envall <jimmyenvall@gmail.com>
2019-02-26 17:15:47 +0200

    Removed unecessary flags from build and fixed button position for web build.

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,15 +1,14 @@
 #include "UI.h"
 
 constexpr const Float WidgetHeight{36.0f};
-
 constexpr const Vector2 ButtonSize{120.0f, WidgetHeight};
 
 UiPlane::UiPlane(Ui::UserInterface &ui) :
         Ui::Plane{ui, Ui::Snap::Top | Ui::Snap::Bottom | Ui::Snap::Left | Ui::Snap::Right, 0, 50, 640},
         toggleVertexMarkersButton{*this,
-                                  {Ui::Snap::Top | Ui::Snap::Left, Range2D::fromSize(Vector2::yAxis(0), ButtonSize)},
+                                  {Ui::Snap::Top | Ui::Snap::Left, Range2D::fromSize(Vector2::yAxis(-WidgetHeight), ButtonSize)},
                                   "Toggle markers", Ui::Style::Default},
-        solveButton{*this, {Ui::Snap::Right, toggleVertexMarkersButton, ButtonSize},
+        solveButton{*this, {Ui::Snap::Bottom, toggleVertexMarkersButton, ButtonSize},
                     "Solve", Ui::Style::Default}
 {
 
