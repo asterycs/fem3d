86826e24813a1ddc72068ec88516714fc7c915d5
Henrik Nortamo <henrik.nortamo@aalto.fi>
2019-02-28 16:16:57 +0200

    Mesh boundry are now exported

--- writettg.m
+++ writettg.m
@@ -1,16 +1,19 @@
-function writettg(vs,ts,filename)
+function writettg(vs,ts,bs,filename)
 
 fid=fopen(filename,'w');
 
 assert(size(vs,1) == 3)
 assert(size(ts,1) == 4)
+assert(size(bs,1) == 1)
 
 vertices = size(vs,2);
 tetras = size(ts,2);
+boundry_nodes = size(bs,2);
 
 fprintf(fid, ['d ' num2str(3) '\n']);
 fprintf(fid, ['v ' num2str(vertices) '\n']);
 fprintf(fid, ['t ' num2str(tetras) '\n']);
+fprintf(fid, ['b ' num2str(boundry_nodes) '\n' ]);
 
 for v=vs
     x = num2str(v(1));
@@ -27,6 +30,11 @@ for t=ts
     v4 = num2str(t(4)-1);
     
     fprintf(fid, [v1 ' ' v2 ' ' v3 ' ' v4 '\n']);
+end
+
+for b=bs
+	idx=num2str(b-1);
+	fprintf(fid,[idx '\n']);
 end
 
 fclose(fid);
