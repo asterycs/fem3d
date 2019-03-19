cf09f1622458321085fbf908ecf3ed2a99c55705
Jimmy Envall <jimmyenvall@gmail.com>
2019-03-19 15:10:56 +0200

    Added energy computation.

--- matlab_files/nonlinear.m
+++ matlab_files/nonlinear.m
@@ -3,7 +3,7 @@ clear all; close all;
 mesh = make_rect_mesh(3);
 
 % Compute initial guess
-bilin = @(U,V,dU,dV,gX)(dU{1}.*dV{1} + dU{2}.*dV{2} );
+bilin = @(U,V,dU,dV,gX)(dU{1}.*dV{1} + dU{2}.*dV{2});
 linf = @(V,dV,gX)(sin(pi*gX{1}).*sin(pi*gX{2}).*V);
 [A,b] = simple_assembly(mesh,bilin,linf);
 
@@ -16,7 +16,7 @@ x = zeros( size(mesh.p,2),1);
 x(iind) = A(iind,iind)\b(iind);
 
 % Set up
-gamma = 1;
+gamma = 0.1;
 maxiter = 1000;
 epsnorms = zeros(maxiter,1);
 energies = zeros(maxiter,1);
@@ -24,13 +24,13 @@ energies = zeros(maxiter,1);
 
 % a = @(x) 1./(1+exp(-x));
 % da = @(x) exp(-x)./(exp(-x)+1).^2;
-a = @(x) 1+x.^2;
-da = @(x) 2*x;
+a = @(x) 1;
+da = @(x) 0;
 bilin_eps_wrapped = @(E,V,dE,dV,gX,dU,dUNorm2) bilin_eps(E,V,dE,dV,gX,dU,dUNorm2,a,da);
 G = @(U,V,dU,dV,gX,dUNorm2) G_eps(U,V,dU,dV,gX,dUNorm2,a,da);
 
-J = @(u,du,gX,detA) 1/2*sum(G(u,u,du,du,gX)*W.*abs(detA))-sum(linf(u,du,gX)*W.*abs(detA));
-
+J = @(u,du,gX,dUNorm2,detA,W) 1/2*sum(G(u,u,du,du,gX,dUNorm2)*W.*abs(detA))-sum(linf(u,du,gX)*W.*abs(detA));
+    
 for i=1:maxiter
     
     [A,b] = step_assembly(mesh,bilin_eps_wrapped,linf,G,x);
@@ -39,14 +39,16 @@ for i=1:maxiter
     epsilon(iind) = A(iind,iind)\b(iind);
     
     % Evaluate ||epsilon|| on every step
-    [epsilon_evaluated, depsilon_evaluated, epsilon_gx] = eval2Dtri(mesh, epsilon, [0.5 0.5 0; 0 0.5 0.5]);
+    xip = [0.5 0.5 0; 0 0.5 0.5];
     W = ones(3,1)*1/6;
-    epsilon_l2norm = sqrt(sum(epsilon_evaluated.^2*W.*abs(detA)));
+    [epsilon_eval, depsilon_eval, epsilon_gx] = eval2Dtri(mesh, epsilon, xip);
+    epsilon_l2norm = sqrt(sum(epsilon_eval.^2*W.*abs(detA)));
     epsnorms(i) = epsilon_l2norm;
     
     % And energy
-    energy = J(epsilon_evaluated, depsilon_evaluated, epsilon_gx, detA);
-    energies(end+1) = energy;
+    dEpsilonNorm2 = sum((depsilon_eval{1}.^2+depsilon_eval{2}.^2).^2*W.*abs(detA));
+    energy = J(epsilon_eval, depsilon_eval, epsilon_gx, dEpsilonNorm2, detA,W);
+    energies(i) = energy;
     
     x = x + gamma*epsilon;
 end
@@ -59,3 +61,9 @@ plot(epsnorms);
 grid on;
 xlabel('Iteration (1)');
 ylabel('||\epsilon|| (1)');
+
+figure;
+plot(energies);
+grid on;
+xlabel('Iteration (1)');
+ylabel('J (1)');
