6bbbcbc1b771c5e979ea8153e7d0c6cee8198476
Jimmy Envall <jimmyenvall@gmail.com>
2019-04-02 15:25:20 +0300

    Fixed energy.

--- matlab_files/energy.m
+++ matlab_files/energy.m
@@ -9,7 +9,7 @@ W = ones(3,1)*1/6;
 
 [U, dU, gX] = eval2Dtri(mesh, x, xip);
 
-dUNorm2 = sum((dU{1}.^2+dU{2}.^2)*W.*abs(detA));
+dUNorm2 = (dU{1}.^2+dU{2}.^2)*W.*abs(detA);
 
 J = 1/2*sum(a(U,U,dU,dU,gX,dUNorm2)*W.*abs(detA))-sum(L(U,dU,gX)*W.*abs(detA));
 
